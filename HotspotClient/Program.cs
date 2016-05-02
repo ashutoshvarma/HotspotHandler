e554c0861593ccdff4e97acc43c8d3ff1f1c4715
Ashutosh Varma <ashutoshvarma11@live.com>
2016-05-02 17:11:10 +0530

    Added CrashReporter.NET to handle all exceptions

--- HotspotClient/Program.cs
+++ HotspotClient/Program.cs
@@ -35,11 +35,43 @@ namespace HotspotClient
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
+
         static void Main()
         {
+
+            Application.ThreadException += ApplicationThreadException;
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
+
+
+        private static void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs)
+        {
+            ReportCrash((Exception)unhandledExceptionEventArgs.ExceptionObject);
+            Environment.Exit(0);
+        }
+
+        private static void ApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            ReportCrash(e.Exception);
+        }
+
+        private static void ReportCrash(Exception exception)
+        {
+            var reportCrash = new ReportCrash
+            {
+                ToEmail = "varma.inc@outlook.com"
+            };
+
+            reportCrash.Send(exception);
+        }
+
+
     }
 }
